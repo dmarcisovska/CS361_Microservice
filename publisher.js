d9d47b4eeef4c4f161f816b83b4b2e92e4090647
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-10-30 16:44:52 -0700

    added a couple more date formats

--- publisher.js
+++ publisher.js
@@ -7,21 +7,29 @@ async function run() {
   console.log("Publisher bound to port 3000");
 
   while (true) {
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
     const currentDate = new Date().toLocaleString();
     const today = new Date();
     const year = today.getFullYear();
     const month = today.getMonth() + 1;
+    const month_written = monthNames[today.getMonth()]; 
     const date = today.getDate();
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
+    
     const messages = {
       current_date: currentDate,
       current_year: year.toString(),
       current_month: month.toString(),
       current_day: date.toString(),
+      current_date_formatted: `${month_written} ${date}, ${year}`,
+      current_date_slashes_formatted: `${month}/${date}/${year}`,
       current_time: `${hours}:${minutes}:${seconds}`,
       current_hours: hours.toString(),
       current_minutes: minutes.toString(),
--- subscriber.js
+++ subscriber.js
@@ -1,20 +1,31 @@
-const zmq = require("zeromq")
+const zmq = require("zeromq");
 
 async function run() {
-  const sock = new zmq.Subscriber()
+  const sock = new zmq.Subscriber();
 
-  sock.connect("tcp://127.0.0.1:3000")
-  sock.subscribe("kitty cats")
-  console.log("Subscriber connected to port 3000")
+  sock.connect("tcp://127.0.0.1:3000");
+
+  //subscriber options
+  // sock.subscribe("current_date");
+  // sock.subscribe("current_year");
+  // sock.subscribe("current_month");
+  // sock.subscribe("current_day");
+  // sock.subscribe("current_time");
+  sock.subscribe("current_date_formatted");
+  sock.subscribe("current_date_slashes_formatted");
+  // sock.subscribe("current_hours");
+  // sock.subscribe("current_minutes");
+  // sock.subscribe("current_seconds");
+
+  console.log("Subscriber connected to port 3000");
 
   for await (const [topic, msg] of sock) {
     console.log(
-      "received a message related to:",
-      topic,
-      "containing message:",
-      msg,
-    )
+      topic.toString(),
+      ":",
+      msg.toString()
+    );
   }
 }
 
-run()
+run();
