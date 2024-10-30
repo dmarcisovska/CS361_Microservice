8a9381173934471f5fe50b4e1cafbbfd06915e67
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-10-30 15:51:52 -0700

    formatted with prettier, added new variables so can send current date and time in increments to user

--- publisher.js
+++ publisher.js
@@ -1,19 +1,27 @@
-const zmq = require("zeromq")
+const zmq = require("zeromq");
 
 async function run() {
-  const sock = new zmq.Publisher()
+  const sock = new zmq.Publisher();
 
-  await sock.bind("tcp://127.0.0.1:3000")
-  console.log("Publisher bound to port 3000")
+  await sock.bind("tcp://127.0.0.1:3000");
+  console.log("Publisher bound to port 3000");
 
   while (true) {
     const currentDate = new Date().toLocaleString();
-    console.log("sending the current date and time")
-    await sock.send(["current_time", currentDate])
-    await new Promise(resolve => {
-      setTimeout(resolve, 500)
-    })
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    console.log("sending the current date and time");
+    await sock.send(["current_time", currentDate]);
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500);
+    });
   }
 }
 
-run()
+run();
